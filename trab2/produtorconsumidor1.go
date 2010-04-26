a4d6c7545e44a5eda6f46f564fa352eba4bda39a
André Ferreira <greiskul@gmail.com>
2010-04-26 18:23:33 -0300

    Argumentos do programa, com numero de produtores, consumidores e tamanho do buffer

--- trab2/produtorconsumidor1.go
+++ trab2/produtorconsumidor1.go
@@ -4,27 +4,40 @@ import (
   "fmt"
   "rand" 
   "time"
+  "flag"
 )
 
 func main() {
-  var canal = make(chan int, 3)
-  for i := 0; i < 2; i++ {
-    var threadnumber = i
-    go func () {
-      for {
-        var temp = rand.Intn(100);
-        fmt.Printf("Thread %d enviando %d\n",threadnumber,temp);
-        canal <- temp
-        fmt.Printf("Thread %d enviado\n",threadnumber);
-        time.Sleep(rand.Int63n(5000))
-      } 
-    } ()
-  }
-  var consumidor = func () {
-    for {
-      fmt.Printf("%d recebido\n",<-canal);
-      time.Sleep(rand.Int63n(5000))
-    }
-  }
-  consumidor()
+	var consumers *int = flag.Int("c", -1, "number of consumers, must be bigger than 0")
+	var producers *int = flag.Int("p", -1, "number of producers, must be bigger than 0")
+	var buffersize *int = flag.Int("b", -1, "size of buffer, must be bigger or equal to 0")
+	flag.Parse()
+	if *consumers <= 0 || *producers <= 0 || *buffersize < 0 {
+		flag.PrintDefaults()
+		return
+	}
+	var canal = make(chan int, *buffersize)
+	for i := 0; i < *producers; i++ {
+		var threadnumber = i
+		go func () {
+		  for {
+			var temp = rand.Intn(100);
+			fmt.Printf("Thread %d enviando %d\n",threadnumber,temp);
+			canal <- temp
+			fmt.Printf("Thread %d enviado\n",threadnumber);
+			time.Sleep(rand.Int63n(5000))
+		  } 
+		} ()
+	}
+	for i := 0; i < *consumers; i++ {
+	  var threadnumber = i + *producers
+	  go func () {
+		for {
+		  fmt.Printf("Thread %d recebeu %d\n",threadnumber,<-canal);
+		  time.Sleep(rand.Int63n(5000))
+		}
+	  } ()
+	}
+	var quit = make(chan int)
+	<- quit
 } 
